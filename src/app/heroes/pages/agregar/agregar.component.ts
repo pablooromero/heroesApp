67b3dbcf0525b56851209167458d0638863e148f
Pablo <pabloromerook@gmail.com>
2021-11-07 23:04:06 -0300

    agregar actualizado

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -1,16 +1,39 @@
 import { Component, OnInit } from '@angular/core';
+import { Heroe, Publisher } from '../../interfaces/heroes.interface';
 
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
-  ]
+  styles: [],
 })
 export class AgregarComponent implements OnInit {
+  publishers = [
+    {
+      id: 'DC Comics',
+      desc: 'DC-Comics',
+    },
+    {
+      id: 'Marvel Comics',
+      desc: 'Marvel-Comics',
+    },
+  ];
 
-  constructor() { }
+  heroe: Heroe = {
+    superhero: '',
+    alter_ego: '',
+    characters: '',
+    first_appearance: '',
+    publisher: Publisher.DCComics,
+    alt_img: '',
+  };
 
-  ngOnInit(): void {
+  constructor() {}
+
+  ngOnInit(): void {}
+
+  guardar() {
+    if (this.heroe.superhero.trim().length === 0) {
+      return;
+    }
   }
-
 }
